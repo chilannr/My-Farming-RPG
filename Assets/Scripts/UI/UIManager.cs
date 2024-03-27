c22aa49c639ccf859469f87a6df421754da51990
chilannairuo <593842215@qq.com>
2024-03-27 21:53:49 +0800

    UI PauseMenu

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,120 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : SingletonMonobehaviour<UIManager>
+{
+    // 暂停菜单是否打开
+    private bool _pauseMenuOn = false;
+
+    //[SerializeField] private UIInventoryBar uiInventoryBar = null;
+    //[SerializeField] private PauseMenuInventoryManagement pauseMenuInventoryManagement = null;
+    [SerializeField] private GameObject pauseMenu = null;
+    [SerializeField] private GameObject[] menuTabs = null;
+    [SerializeField] private Button[] menuButtons = null;
+
+    public bool PauseMenuOn { get => _pauseMenuOn; set => _pauseMenuOn = value; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        // 初始化时将暂停菜单设为非活动状态
+        pauseMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        PauseMenu();
+    }
+
+    private void PauseMenu()
+    {
+        // 按下Esc键时切换暂停菜单状态
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PauseMenuOn)
+            {
+                DisablePauseMenu();
+            }
+            else
+            {
+                EnablePauseMenu();
+            }
+        }
+    }
+
+    private void EnablePauseMenu()
+    {
+        //uiInventoryBar.DestroyCurrentlyDraggedItems();
+        //uiInventoryBar.ClearCurrentlySelectedItems();
+
+        PauseMenuOn = true;
+        Player.Instance.PlayerInputIsDisabled = true;
+        Time.timeScale = 0; // 暂停游戏时间
+        pauseMenu.SetActive(true);
+
+        System.GC.Collect(); // 触发垃圾回收
+
+        HighlightButtonForSelectedTab();
+    }
+
+    public void DisablePauseMenu()
+    {
+        //pauseMenuInventoryManagement.DestroyCurrentlyDraggedItems();
+
+        PauseMenuOn = false;
+        Player.Instance.PlayerInputIsDisabled = false;
+        Time.timeScale = 1; // 恢复游戏时间
+        pauseMenu.SetActive(false);
+    }
+
+    private void HighlightButtonForSelectedTab()
+    {
+        for (int i = 0; i < menuTabs.Length; i++)
+        {
+            if (menuTabs[i].activeSelf)
+            {
+                SetButtonColorToActive(menuButtons[i]);
+            }
+            else
+            {
+                SetButtonColorToInactive(menuButtons[i]);
+            }
+        }
+    }
+
+    private void SetButtonColorToActive(Button button)
+    {
+        ColorBlock colors = button.colors;
+        colors.normalColor = colors.pressedColor;
+        button.colors = colors;
+    }
+
+    private void SetButtonColorToInactive(Button button)
+    {
+        ColorBlock colors = button.colors;
+        colors.normalColor = colors.disabledColor;
+        button.colors = colors;
+    }
+
+    public void SwitchPauseMenuTab(int tabNum)
+    {
+        for (int i = 0; i < menuTabs.Length; i++)
+        {
+            if (i != tabNum)
+            {
+                menuTabs[i].SetActive(false);
+            }
+            else
+            {
+                menuTabs[i].SetActive(true);
+            }
+        }
+
+        HighlightButtonForSelectedTab();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
